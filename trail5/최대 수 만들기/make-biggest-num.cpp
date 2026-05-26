4a8c0cd03e44fc3cb03da9e6fc0a982cba276fec
crueusNB <crueusnote@gmail.com>
2026-05-26 10:24:47 +0900

    [Runtime Error] 최대 수 만들기 (6ms, 1MB)

---
+++ trail5/최대 수 만들기/make-biggest-num.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector<string> vec;
+
+bool compare(string a, string b){
+    string tmp1 = a + b;
+    string tmp2 = b + a;
+
+    if (stoi(tmp1) > stoi(tmp2))
+        return true;
+    return false;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n ; i++){
+        string str;
+        cin >> str;
+        vec.push_back(str);
+    }
+
+    sort(vec.begin(), vec.end(), compare);
+
+    for (int i = 0; i < n; i++){
+        cout << vec[i];
+    }
+    return 0;
+}

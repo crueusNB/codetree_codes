7f56991c74f5d3dc11a758ccc52b60bd06cfe5ff
crueusNB <crueusnote@gmail.com>
2026-09-15 13:30:01 +0900

    [Wrong Answer] 가장 오래 걸리는 학생 2 (124ms, 9MB)

--- trail5/가장 오래 걸리는 학생 2/longest-student-2.cpp
+++ trail5/가장 오래 걸리는 학생 2/longest-student-2.cpp
@@ -11,7 +11,7 @@ int N, M;
 int i[100000], j[100000], d[100000];
 const int INF = 2000000000;
 priority_queue<pair<int, int>> pq;
-vector<pair<int, int>> edges[100001];
+vector<pair<int, int>> edges[200001];
 int dist[100001];
 
 int main() {
